5e363438fb256b3c7f9cf0240388436483175318
samridh-dev <singhsam@grinnell.edu>
2025-05-27 12:31:03 -0500

    updated param.hpp

--- include/param.hpp
+++ include/param.hpp
@@ -1,18 +1,25 @@
 #ifndef PARAM_HPP
 #define PARAM_HPP
 
+/* ------------------------------------------------------------------------- */
+// Type Parameters
 using I = std::size_t;
-
 using Tf = float;
 using Ti = int;
+/* ------------------------------------------------------------------------- */
 
+/* ------------------------------------------------------------------------- */
+// Votess parameters
 constexpr I k   = 64;
 constexpr I gr  = 32;
+/* ------------------------------------------------------------------------- */
 
-constexpr I imax = 1e0;
-
-constexpr I N0 = 100;
-constexpr I N1 = 100000;
-constexpr I dN = 100;
+/* ------------------------------------------------------------------------- */
+// Benchmark Parameters
+constexpr I imax = 1e0;        // Maximum number of iterations per step
+constexpr I N0   = 1e2;        // Initial dataset size
+constexpr I N1   = 1e5;        // Final   dataset size
+constexpr I dN   = 1e2;        // Initial dataset step (increases over time)
+/* ------------------------------------------------------------------------- */
 
 #endif PARAM_HPP
