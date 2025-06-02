99786eb4ef3b5f3e99a6531143b77490cf1ae399
samridh-dev <singhsam@grinnell.edu>
2025-06-02 10:42:42 -0500

    updated params

--- include/param.hpp
+++ include/param.hpp
@@ -18,7 +18,7 @@ constexpr I gr  = 32;
 // Benchmark Parameters
 constexpr I imax = 1e0;        // Maximum number of iterations per step
 constexpr I N0   = 1e2;        // Initial dataset size
-constexpr I N1   = 1e5;        // Final   dataset size
+constexpr I N1   = 1e6;        // Final   dataset size
 constexpr I dN   = 1e2;        // Initial dataset step (increases over time)
 /* ------------------------------------------------------------------------- */
 
