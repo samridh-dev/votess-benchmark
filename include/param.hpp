949c047a4e60ecc18e91707343bbffe0141eda60
samridh-dev <singhsam@grinnell.edu>
2025-06-19 00:00:25 -0500

    removed dynamic variables

--- include/param.hpp
+++ include/param.hpp
@@ -3,27 +3,9 @@
 
 /* ------------------------------------------------------------------------- */
 // Type Parameters
-using I = std::size_t;
+using I =  int;
 using Tf = float;
 using Ti = int;
-/* ------------------------------------------------------------------------- */
-
-/* ------------------------------------------------------------------------- */
-// Votess parameters
-constexpr I k   = 64;
-constexpr I gr  = 32;
-/* ------------------------------------------------------------------------- */
-
-/* ------------------------------------------------------------------------- */
-// Benchmark Parameters
-constexpr I imax = 1e0;           // Maximum number of iterations per step
-constexpr I N0   = 1e3;           // Initial dataset size
-constexpr I N1   = 1e6;           // Final   dataset size
-
-constexpr I counts = 1e3;         // Total number of data points to collect
-
-constexpr I dN     = N1 / counts; // Initial dataset step (increases over time)
-
 /* ------------------------------------------------------------------------- */
 
 #endif PARAM_HPP
